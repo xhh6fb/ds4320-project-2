5ee0b30ce70b8176d44682e278cc47ce0ca759bd
xhh6fb <157515386+xhh6fb@users.noreply.github.com>
2026-04-28 19:56:47 -0400

    Update sample_mongosh_queries.js

--- src/sample_mongosh_queries.js
+++ src/sample_mongosh_queries.js
@@ -5,17 +5,33 @@ db.qb_game_docs.countDocuments()
 db.qb_game_docs.findOne()
 
 db.qb_game_docs.find(
-  { season: 2024, "player_info.team": "BUF" },
-  { "player_info.player_name": 1, week: 1, targets: 1 }
+  { season: 2024 },
+  {
+    "player_info.player_name": 1,
+    "player_info.team": 1,
+    week: 1,
+    "targets.passing_yards": 1,
+    "targets.passing_tds": 1
+  }
 ).limit(5)
+
+db.qb_game_docs.find(
+  { "player_info.player_name": "Josh Allen" },
+  {
+    season: 1,
+    week: 1,
+    "pregame_form.avg_pass_yards_last_3": 1,
+    "targets.passing_yards": 1
+  }
+).sort({ season: 1, week: 1 }).limit(10)
 
 db.qb_game_docs.aggregate([
   {
     $group: {
       _id: "$season",
-      avg_pass_yards: { $avg: "$targets.passing_yards" },
-      avg_pass_tds: { $avg: "$targets.passing_tds" },
-      n_docs: { $sum: 1 }
+      doc_count: { $sum: 1 },
+      avg_passing_yards: { $avg: "$targets.passing_yards" },
+      avg_passing_tds: { $avg: "$targets.passing_tds" }
     }
   },
   { $sort: { _id: 1 } }
