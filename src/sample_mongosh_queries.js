84debdf022545849755a0bc99053bf2c01b49c41
xhh6fb <157515386+xhh6fb@users.noreply.github.com>
2026-04-28 21:30:49 -0400

    Update sample_mongosh_queries.js

--- src/sample_mongosh_queries.js
+++ src/sample_mongosh_queries.js
@@ -1,38 +1,26 @@
-use ds4320_project2
+// switch database
+use project2_db
 
-db.qb_game_docs.countDocuments()
+// view documents
+db.qb_games.find().limit(5)
 
-db.qb_game_docs.findOne()
+// filter by QB
+db.qb_games.find({
+  "player_info.player_name": "Patrick Mahomes"
+})
 
-db.qb_game_docs.find(
-  { season: 2024 },
-  {
-    "player_info.player_name": 1,
-    "player_info.team": 1,
-    week: 1,
-    "targets.passing_yards": 1,
-    "targets.passing_tds": 1
-  }
-).limit(5)
+// high yard games
+db.qb_games.find({
+  "targets.passing_yards": { $gt: 300 }
+})
 
-db.qb_game_docs.find(
-  { "player_info.player_name": "Josh Allen" },
-  {
-    season: 1,
-    week: 1,
-    "pregame_form.avg_pass_yards_last_3": 1,
-    "targets.passing_yards": 1
-  }
-).sort({ season: 1, week: 1 }).limit(10)
-
-db.qb_game_docs.aggregate([
+// aggregation example
+db.qb_games.aggregate([
   {
     $group: {
-      _id: "$season",
-      doc_count: { $sum: 1 },
-      avg_passing_yards: { $avg: "$targets.passing_yards" },
-      avg_passing_tds: { $avg: "$targets.passing_tds" }
+      _id: "$player_info.player_name",
+      avg_yards: { $avg: "$targets.passing_yards" }
     }
   },
-  { $sort: { _id: 1 } }
+  { $sort: { avg_yards: -1 } }
 ])
