5eb8d7ddef8e47d9a43a526f0c12c1e808c3126e
xhh6fb <157515386+xhh6fb@users.noreply.github.com>
2026-04-28 21:43:09 -0400

    Update load_project2_to_mongo.py

--- src/load_project2_to_mongo.py
+++ src/load_project2_to_mongo.py
@@ -2,36 +2,26 @@ from pymongo import MongoClient
 import json
 from utils_project2 import setup_logger
 
-# -----------------------------------------
-# SETUP LOGGER
-# -----------------------------------------
 logger = setup_logger("mongo_load.log")
 
 try:
     logger.info("Connecting to MongoDB")
 
-    client = MongoClient("mongodb+srv://<username>:<password>@cluster0.6mite1b.mongodb.net/?appName=Cluster0")
+    client = MongoClient("mongodb+srv://xhh6fb:xv63vmjk@cluster0.6mite1b.mongodb.net/?appName=Cluster0")
 
     db = client["project2_db"]
     collection = db["qb_games"]
 
-    # -----------------------------------------
-    # LOAD JSON FILE
-    # -----------------------------------------
     with open("data/qb_documents.json") as f:
         docs = json.load(f)
 
-    logger.info(f"Loaded {len(docs)} documents")
+    logger.info(f"Loaded {len(docs)} docs")
 
-    # -----------------------------------------
-    # INSERT INTO DATABASE
-    # -----------------------------------------
     collection.delete_many({})
     collection.insert_many(docs)
 
-    logger.info("Data successfully inserted into MongoDB")
+    logger.info("Upload complete")
 
 except Exception as e:
-    logger.error("MongoDB ERROR")
     logger.error(str(e))
     raise
